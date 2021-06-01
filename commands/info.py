78647892b3f49e99de7ba25bd5f344b9565277cc
Aaron <dougdeenaaron@gmail.com>
2021-06-01 00:22:37 -0400

    Waifu and HighLow command update

--- commands/info.py
+++ commands/info.py
@@ -12,7 +12,9 @@ class Info(commands.Cog):
                                  color=0x00ff00)
         embedVar.add_field(name='Commands', value="**!info**  - description of bot and list of commands\n\n"
                                                   + "**!quote** - random dumb quote\n\n"
-                                                  + "**!highlow** - higher or lower game for currency", inline=False)
+                                                  + "**!highlow** - higher or lower game for currency\n\n"
+                                                    "**!waifu roll - roll waifu\n\n"
+                                                    "**!waifu list -  lists your claimed characters", inline=False)
         embedVar.set_footer(text='Created by SacLamb',
                             icon_url='https://cdn.discordapp.com/avatars/154690746107035648/a_985713092df32da3a7af0fd605e83e6d.gif?size=128')
         await ctx.send(embed=embedVar)
